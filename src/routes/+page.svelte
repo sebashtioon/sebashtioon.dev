c13b2ccc1918e8ce5a467671e82b6dd67ad92a60
sebashtioon <sebastiansuciu607@gmail.com>
2025-03-19 13:34:29 +1100

    Update page title and add button components to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>sebashtioon.dev</h1>
+
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+   </script>
+    
+   <Button>This is cool buton</Button>
+   <Button variant="secondary">this is secondary button</Button>
+   <Button variant="destructive">bad button</Button>
+   <Button variant="outline" class="text-xl">outlin batton</Button>
